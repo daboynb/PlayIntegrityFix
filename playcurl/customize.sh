627929a517b9f2531be7297917e794e580489a59
daboynb <106079917+daboynb@users.noreply.github.com>
2024-05-24 18:16:45 +0200

    Revert "Merge pull request #4 from vladrevers/patch-1"
    This reverts commit e9576f8da67f828ed253a414aa2463090b27430b, reversing
    changes made to e047abac7b83e6c53c534c90b425e30063cb5301.

--- playcurl/customize.sh
+++ playcurl/customize.sh
@@ -5,7 +5,6 @@ set_perm $MODPATH/system/bin/fpa root root 0777
 set_perm $MODPATH/system/bin/fpt root root 0777
 mv -f $MODPATH/system/bin/$ABI/curl $MODPATH/system/bin
 set_perm $MODPATH/system/bin/curl root root 777
-rm -r $MODPATH/system/bin/arm64-v8a $MODPATH/system/bin/armeabi-v7a $MODPATH/system/bin/x86 $MODPATH/system/bin/x86_64
 
 echo ""
 ui_print "You can edit /data/adb/modules/playcurl/seconds.txt"
