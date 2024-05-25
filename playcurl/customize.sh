fd30e8e29363c8d0d592618e5bc5b346b6ef1aad
daboynb <106079917+daboynb@users.noreply.github.com>
2024-05-25 14:49:39 +0200

    Update customize.sh

--- playcurl/customize.sh
+++ playcurl/customize.sh
@@ -9,7 +9,7 @@ set_perm $MODPATH/system/bin/curl root root 777
 
 rm -rf $MODPATH/system/bin/arm64-v8a 
 rm -rf $MODPATH/system/bin/armeabi-v7a 
-rm -rf MODPATH/system/bin/x86 
+rm -rf $MODPATH/system/bin/x86 
 rm -rf $MODPATH/system/bin/x86_64
 
 echo ""
